6ce360af4d5b1c95783429609e05512ddae3b84c
MuhammedKK <khaloood2281999@gmail.com>
2023-10-19 04:11:16 +0300

    fix all esling errors

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,30 +1,6 @@
-{/* @ts-ignore */}
-import {
-    mobile,
-    backend,
-    creator,
-    web,
-    javascript,
-    typescript,
-    html,
-    css,
-    reactjs,
-    redux,
-    tailwind,
-    nodejs,
-    mongodb,
-    git,
-    figma,
-    docker,
-    meta,
-    starbucks,
-    tesla,
-    shopify,
-    carrent,
-    jobit,
-    tripguide,
-    threejs,
-  } from '../assets';
+/* @ts-ignore */
+import {mobile, backend, creator, web, javascript, typescript, html, css, reactjs, redux, tailwind, nodejs, mongodb, git, mongodb, figma, docker, meta, starbucks, tesla, shopify, carrent, jobit, tripguide, threejs} from "../assets"; 
+  
   
   const navLinks = [
     {
